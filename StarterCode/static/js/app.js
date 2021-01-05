a310ac27647d621f3a156787503188c32428d2d0
Serapbasaran <serap.cbasaran@gmail.com>
2021-01-04 23:21:27 -0500

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -83,11 +83,11 @@ function init (){
 
 // Display the sample metadata for each UTO_ID selected;
 
-function metaData(sample) {
+function metaData(sampleID) {
     d3.json("samples.json").then((data) => {
       var metadata = data.metadata;
       // Filter the data for the object with the desired sample number
-      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sampleID);
       var result = resultArray[0];
       // Use d3 to select the panel with id of `#sample-metadata`
       var PANEL = d3.select("#sample-metadata");
