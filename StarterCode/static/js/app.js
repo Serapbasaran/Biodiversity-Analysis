677d33722e32d61eb216b5690deaf2d1aea5b89c
Serapbasaran <serap.cbasaran@gmail.com>
2021-01-04 23:19:30 -0500

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,15 +1,105 @@
 // Set up the initial position of the page by sample IDs to the dropdown menu
+function init (){
+    var dropdown = d3.selectAll('#selDataset');
+
+    d3.json('samples.json').then((data)=>{
+    
+        var sampleNames=data.names;
+        sampleNames.forEach((sample) => {
+            dropdown
+              .append("option")
+              .text(sample)
+              .property("value", sample);
+            });
+      
+        var initSample = sampleNames[0];
+    
+        barChart(initSample);
+        bubbleChart(initSample);
+        metaData(initSample);
+
+      });
+     };
+    function optionChanged(newSample) {
+        // Fetch new data each time a new sample is selected
+        barChart(newSample);
+        bubbleChart(newSample);
+        metaData(newSample)
+    };
+    init ();
 
 
+    function barChart(sampleId){
+        d3.json('samples.json').then((data)=>{
+            var samples=data.samples;
+            var testNum=samples.map(row=>row.id).indexOf(sampleId);
+            console.log(testNum)
+            var otuValueTen=samples.map(row=>row.sample_values);
+            var otuValueTen=otuValueTen[testNum].slice(0,10).reverse();
+            var otuIdTen=samples.map(row=>row.otu_ids);
+            var otuIdTen=otuIdTen[testNum].slice(0,10);
+            var otuLabelTen=samples.map(row=>row.otu_labels).slice(0,10);
+            
+            var trace={
+                x: otuValueTen,
+                y: otuIdTen.map(r=>`UTO ${r}`),
+                text: otuLabelTen,
+                type:'bar',
+                orientation:'h'
+            }
+            Plotly.newPlot('bar',[trace]);
+        })
+    };
+    
+    function bubbleChart(sampleID){
+        d3.json('samples.json').then((data)=>{
+            var samples=data.samples;
+            var testNum=samples.map(row=>row.id).indexOf(sampleID);
+            var otu_ids = samples.map(row => row.otu_ids);
+            var otu_ids = otu_ids[testNum];
+            console.log(otu_ids)
+            var sample_values = samples.map(row => row.sample_values);
+            var sample_values = sample_values[testNum];
+            var otu_labels = samples.map(row => row.otu_labels);
+            var otu_labels = otu_labels[testNum];
+            var trace1 = {
+                x: otu_ids,
+                y: sample_values,
+                text: otu_labels,
+                mode: 'markers',
+                marker: {size: sample_values, 
+                         color: otu_ids
+                        }
+                };                       
+                
+            var layout = { xaxis: {title: 'OTU ID'},
+                           height: 600,
+                           width: 1000
+                         };
+            Plotly.newPlot('bubble',[trace1], layout);
+        })
+    };
 
 
-d3.json('samples.json').then((data)=>{
-    var id=data.names;
-    console.log(data.metadata);
-    var dropdownmenu = d3.selectall('#selDataset');
+// Display the sample metadata for each UTO_ID selected;
 
-    Object.entries(id).forEach(([i,v])=>{
-        dropdownmenu.append('option').text(v);
-    })
-})
-
+function metaData(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      // Filter the data for the object with the desired sample number
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      // Use d3 to select the panel with id of `#sample-metadata`
+      var PANEL = d3.select("#sample-metadata");
+  
+      // Use `.html("") to clear any existing metadata
+      PANEL.html("");
+  
+      // Use `Object.entries` to add each key and value pair to the panel
+      // Hint: Inside the loop, you will need to use d3 to append new
+      // tags for each key-value in the metadata.
+      Object.entries(result).forEach(([key, value]) => {
+        PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+      });
+    });
+}  
