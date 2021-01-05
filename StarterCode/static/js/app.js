202f148286a03a20f87c969cc761bb31ccb55234
Serapbasaran <serap.cbasaran@gmail.com>
2021-01-04 23:30:33 -0500

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -87,7 +87,7 @@ function metaData(sampleID) {
     d3.json("samples.json").then((data) => {
       var metadata = data.metadata;
       // Filter the data for the object with the desired sample number
-      var resultArray = metadata.filter(sampleObj => sampleObj.id == sampleID);
+      var resultArray = metadata.filter(object => object.id == sampleID);
       var result = resultArray[0];
       // Use d3 to select the panel with id of `#sample-metadata`
       var PANEL = d3.select("#sample-metadata");
